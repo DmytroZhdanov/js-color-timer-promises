38b3708a757d8b424a2a66ef1bc8b8c3c0576eeb
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-05-10 15:03:52 +0000

    Deploying to gh-pages from @ DmytroZhdanov/goit-js-hw-09@71c0509e37bb5af196f6e53c4c3dc1f4cb775770 🚀

---
+++ 01-color-switcher.d7b31d39.js
@@ -0,0 +1,2 @@
+const t={body:document.body,startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};let n=null;t.stopBtn.disabled=!0,t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,t.stopBtn.disabled=!1,n=setInterval((()=>{const n=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;t.body.style.backgroundColor=n}),1e3)})),t.stopBtn.addEventListener("click",(function(){t.startBtn.disabled=!1,t.stopBtn.disabled=!0,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.d7b31d39.js.map
