71c0509e37bb5af196f6e53c4c3dc1f4cb775770
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-05-10 18:01:35 +0300

    Solves task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const refs = {
+  body: document.body,
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+let id = null;
 
+refs.stopBtn.disabled = true;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onstopBtnClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function onStartBtnClick() {
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+  id = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    refs.body.style.backgroundColor = randomColor;
+  }, 1000);
+}
+
+function onstopBtnClick() {
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+
+  clearInterval(id);
+}
